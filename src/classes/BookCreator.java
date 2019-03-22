f6a5524ba28ddae10c55371d61846037a1f549ab
jvm <jvm@jvmHome>
2019-03-22 14:08:35 +0200

    Программа дописана и теперь можно вводить количество авторов для каждой книги и выбирать их из списка

--- src/classes/BookCreator.java
+++ src/classes/BookCreator.java
@@ -18,18 +18,29 @@ public class BookCreator {
     public Book create(Author[] authors){
         Book book = new Book();
         System.out.println("-------Создание книги--------");
-        System.out.println("Название книги: ");
+        System.out.print("Название книги: ");
         book.setTitle(scanner.nextLine());
-        System.out.println("Текст книги: ");
+        System.out.print("Текст книги: ");
         book.setText(scanner.nextLine());
-        int numAuthors = 0;
-        System.out.println("Номер автора в массиве: ");
-        numAuthors= scanner.nextInt();
-        book.setAuthors(new Author[]{authors[numAuthors]});
-        System.out.println("Год издания: ");
+        int countAuthors=0;
+        System.out.print("Сколько авторов у книги: ");
+        countAuthors = scanner.nextInt();
+        int n=0;
+        int numAuthor = 0;
+        Author[] authorsInBook = new Author[countAuthors];
+        while(n < countAuthors){
+            this.printAuthorsArray();
+            System.out.println("Номер "+(n+1)+" автора в массиве: ");
+            numAuthor= scanner.nextInt();
+            authorsInBook[n]=authors[numAuthor];
+            n++;
+        }
+        book.setAuthors(authorsInBook);
+        System.out.print("Год издания: ");
         book.setYear(scanner.nextInt());
-        System.out.println("Количество страниц: ");
+        System.out.print("Количество страниц: ");
         book.setPages(scanner.nextInt());
+        scanner.nextLine();
         return book;
     }
     public Book[] creator(){
@@ -41,6 +52,7 @@ public class BookCreator {
             i++;
             System.out.println("Для ввода еще одной книги нажмите r");
             stop = scanner.next();
+            scanner.nextLine();
         }
         return books;
     }
@@ -51,6 +63,12 @@ public class BookCreator {
 
     public void setAuthors(Author[] authors) {
         this.authors = authors;
+    }
+    private void printAuthorsArray(){
+        for(int i=0;i<this.authors.length;i++){
+           System.out.println(i+". "+this.authors[i]); 
+        }
+        
     }
     
 }
